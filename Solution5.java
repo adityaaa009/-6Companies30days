45831405ede421e4eb85fca3e382bc0a743a9d76
Aditya Sharma <114949288+adityaaa009@users.noreply.github.com>
2025-01-01 20:23:22 +0530

    Add files via upload

---
+++ Solution5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution5 {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int k = nums[nums.length >> 1];
+        int ans = 0;
+        for (int v : nums) {
+            ans += Math.abs(v - k);
+        }
+        return ans;
+    }
+}
