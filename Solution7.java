7af6b6f94d871f3f59d6418ca78a4381c7ab489f
Aditya Sharma <114949288+adityaaa009@users.noreply.github.com>
2025-01-02 20:35:08 +0530

    Add files via upload

---
+++ Solution7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int res = 0, i = 0, count = 0, n = nums.length;
+        for (int j = 0; j < n; j++) {
+            if (nums[j] % 2 == 1) {
+                --k;
+                count = 0;
+            }
+            while (k == 0) {
+                k += nums[i++] & 1;
+                ++count;
+            }
+            res += count;
+        }
+        return res;
+    }
+    }
