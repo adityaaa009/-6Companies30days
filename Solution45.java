23b261b9dc873e31c5471e2aa5ebd1655056ebe1
Aditya Sharma <114949288+adityaaa009@users.noreply.github.com>
2025-01-17 22:29:20 +0530

    Add files via upload

---
+++ Solution45.java
@@ -0,0 +1,13 @@
+class Solution45 {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long m = mass;
+        for (int v : asteroids) {
+            if (m < v) {
+                return false;
+            }
+            m += v;
+        }
+        return true;
+    }
+}
